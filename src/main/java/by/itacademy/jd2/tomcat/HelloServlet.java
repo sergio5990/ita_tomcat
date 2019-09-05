2fde40c411a1f78474e696e559420ac132782b44
sergey-kruk <sergey-kruk>
2019-09-06 02:15:16 +0300

    servlet @WebServlet

--- src/main/java/by/itacademy/jd2/tomcat/HelloServlet.java
+++ src/main/java/by/itacademy/jd2/tomcat/HelloServlet.java
@@ -1,12 +1,14 @@
 package by.itacademy.jd2.tomcat;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(name = "HelloServlet", urlPatterns = {"/sayhello"})
 public class HelloServlet extends HttpServlet {
 
     @Override
